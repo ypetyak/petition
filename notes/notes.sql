c949669ba411e2d017c5c58c02aac9563b5f1f82
ypetyak <ypetyak@gmail.com>
2019-05-08 15:48:28 +0200

    deleted redundant parts

--- notes/notes.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS signatures;
-
-CREATE TABLE signature(
-    id SERIAL PRIMARY KEY,
-    first VARCHAR(200) NOT NULL,  -- NOT NULL is a constrain, if it will be empty DB should reject it 
-    last VARCHAR(250) NOT NULL,
-
-)
